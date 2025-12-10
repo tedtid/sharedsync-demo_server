1be68f77fb66d024a495ab650e4c6e51907a9f11
sangjun <tedtid@naver.com>
2025-12-10 15:29:35 +0900

    refactor: 의존성 정리

--- src/main/java/com/sharedsync/demo/domain/note/Note.java
+++ src/main/java/com/sharedsync/demo/domain/note/Note.java
@@ -3,14 +3,8 @@ package com.sharedsync.demo.domain.note;
 import com.sharedsync.shared.annotation.CacheEntity;
 import com.sharedsync.shared.presence.annotation.PresenceRoot;
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
-//@Getter
-//@Setter
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
-//@AllArgsConstructor
-//@Builder
 @CacheEntity
 @PresenceRoot(channel = "note", idField = "id")
 public class Note {
